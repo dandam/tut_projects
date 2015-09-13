6f6608baca3ecab29fcfa5815ed9f99aea25100e
Dan Damkoehler <dandam@gmail.com>
2015-09-13 13:27:44 -0500

    persistence of todo items

--- yomytodo/test/spec/controllers/main.js
+++ yomytodo/test/spec/controllers/main.js
@@ -18,6 +18,22 @@ describe('Controller: MainCtrl', function () {
   }));
 
   it('should have no items to start', function () {
-    expect(MainCtrl.todos.length).toBe(0);
+    expect(scope.todos.length).toBe(0);
   });
+
+  it('should add items to the list', function () {
+	  scope.todo = 'Test 1';
+	  scope.addTodo();
+	  expect(scope.todos.length).toBe(1);
+  });
+  
+  it('should add then remove an item from the list', function () {
+	  scope.todo = 'Test 1';
+	  scope.addTodo();
+	  scope.removeTodo(0);
+	  expect(scope.todos.length).toBe(0);
+  })
+
 });
+
+
